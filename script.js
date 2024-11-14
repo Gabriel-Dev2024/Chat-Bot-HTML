059d926cb6a488944eb5c845289a933cadd1554c
Gabriel-Dev2024 <gabrielprudencio2008@gmail.com>
2024-11-14 17:36:28 -0300

    Update 1

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 const typingForm = document.querySelector('.typing-form');
 const chatList = document.querySelector('.chat-list');
+const toggleThemeButton = document.querySelector('#toggle-theme-button');
 
 let userMessage = null;
 
 const API_KEY = 'AIzaSyBditdh8-JKnA7FkrDyGFicOf4xQOljePs';
 const API_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${API_KEY}`;
+
+const loadLocalStorageData = () => {
+    const savedChats = localStorage.getItem('savedChats');
+    const isLightMode = (localStorage.getItem('themeColor') === 'light-mode');
+
+    document.body.classList.toggle('light-mode', isLightMode);
+    toggleThemeButton.innerHTML = isLightMode ? 'dark-mode' : 'light-mode';
+
+    chatList.innerHTML = savedChats || '';
+    chatList.scrollTo(0, chatList.scrollHeight);
+};
+
+loadLocalStorageData();
 
 const createMessageElement = (content, ...classes) => {
     const div = document.createElement('div');
@@ -13,16 +27,20 @@ const createMessageElement = (content, ...classes) => {
     return div;
 };
 
-const showTypingEffect = (text, textElement) => {
+const showTypingEffect = (text, textElement, incomingMessageDiv) => {
     const words = text.split(' ');
     let currentWordIndex = 0;
     textElement.textContent = '';
 
     const typingInterval = setInterval(() => {
         textElement.textContent += (currentWordIndex === 0 ? '' : ' ') + words[currentWordIndex++];
+        incomingMessageDiv.querySelector('.icon').classList.add('hide');
 
         if (currentWordIndex === words.length) {
             clearInterval(typingInterval);
+            incomingMessageDiv.querySelector('.icon').classList.remove('hide');
+            localStorage.setItem('savedChats', chatList.innerHTML);
+            chatList.scrollTo(0, chatList.scrollHeight);
         };
     }, 75);
 };
@@ -51,10 +69,10 @@ const generateAPIResponse = async (incomingMessageDiv) => {
         const data = await response.json();
 
         // Get the API response text
-        const apiResponse = data?.candidates[0].content.parts[0].text;
+        const apiResponse = data?.candidates[0].content.parts[0].text.replace(/\*\*(.*?)\*\*/g, '$1');
 
         if (apiResponse) {
-            showTypingEffect(apiResponse, textElement);
+            showTypingEffect(apiResponse, textElement, incomingMessageDiv);
         } else {
             textElement.textContent = "Nenhuma resposta recebida"
         }
@@ -80,6 +98,7 @@ const showLoadingAnimation = () => {
     const outGoingMessageDiv = createMessageElement(html, 'incoming', 'loading');
     chatList.appendChild(outGoingMessageDiv);
 
+    chatList.scrollTo(0, chatList.scrollHeight);
     generateAPIResponse(outGoingMessageDiv);
 };
 
@@ -105,7 +124,14 @@ const handleOutGoingChat = () => {
     chatList.appendChild(outGoingMessageDiv);
 
     typingForm.reset();
+    chatList.scrollTo(0, chatList.scrollHeight);
     setTimeout(showLoadingAnimation, 500);
+};
+
+const toggleTheme = () => {
+    const isLightMode = document.body.classList.toggle('light-mode');
+    localStorage.setItem('themeColor', isLightMode ? 'light-mode' : 'dark-mode');
+    toggleThemeButton.innerHTML = isLightMode ? 'dark-mode' : 'light-mode';
 };
 
 typingForm.addEventListener('submit', (e) => {
